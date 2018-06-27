42cba65831fdeb0c19bfb5ee50d915e435d5f042
Andres Lujan <lujana@google.com>
2018-06-27 09:53:24 -0500

    WIP

--- test.py
+++ test.py
@@ -11,3 +11,14 @@ minus_inf = float('-inf')
 # Fixed point
 from decimal import Decimal
 price = Decimal('0.02')
+
+
+text = "This is the way the world ends"
+
+text = 'This is the way the world ends'
+
+text = """This is the wat \n
+the world ends"""
+
+text = '''This is the wat \n
+the world ends'''
