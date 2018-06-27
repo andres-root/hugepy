7f49bb18ceb9631a76bdb63116b21f56a0dcaca9
Andres Lujan <lujana@google.com>
2018-06-27 10:30:57 -0500

    WIP

--- test.py
+++ test.py
@@ -50,3 +50,21 @@ thing = bool('any object')  # True
 all_false = False or 0 or '' or [] or {} or None
 
 all_true = True or 1 or 'hello!' or [1, 2, 3] or {'name': 'James'} or None
+
+
+
+
+
+
+a = 10     # 10
+a += 1     # 11
+a -= 1     # 10
+
+b = a + 1  # 11
+c = a - 1  # 9
+
+d = a * 2  # 20
+e = a / 2  # 5
+f = a % 3  # 1
+g = a ** 2 # 100
+
